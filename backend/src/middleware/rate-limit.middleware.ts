d4a39de38bf27997b990de0feae477194b804fd5
Baher <121636917+Baherbb@users.noreply.github.com>
2024-12-03 14:04:16 +0200

    Update rate-limit.middleware.ts

--- backend/src/middleware/rate-limit.middleware.ts
+++ backend/src/middleware/rate-limit.middleware.ts
@@ -5,8 +5,8 @@ export const apiLimiter = rateLimit({
     windowMs: 15 * 60 * 1000, // 15 minutes
     max: 1000, // Limit each IP to 100 requests per windowMs
     message: 'Too many requests from this IP, please try again later',
-    standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
-    legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+    standardHeaders: true, // Return rate limit info in the RateLimit headers
+    legacyHeaders: false, // Disable the X-RateLimit headers
 });
 
 // Auth endpoints rate limiter 
